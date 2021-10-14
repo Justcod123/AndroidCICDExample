30114df5b5e3e63418b55c25d91c049abd4008e8
Justcod123 <himanshu21justcodenow@gmail.com>
2021-10-14 10:34:58 +0530

    Added Login Test case

---
+++ app/src/androidTest/java/com/example/androidcicdexample/LoginTest.kt
@@ -0,0 +1,49 @@
+package com.example.androidcicdexample
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.filters.SmallTest
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Created by Abhin.
+ */
+@SmallTest
+@RunWith(AndroidJUnit4ClassRunner::class)
+class LoginTest {
+
+    @Before
+    fun start() {
+        ActivityScenario.launch(MainActivity::class.java)
+        Thread.sleep(500)
+    }
+
+    @Test
+    fun loginTest() {
+
+        Espresso.onView(ViewMatchers.withId(R.id.edt_email_main_activity))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed())).perform(
+                ViewActions.typeText("test@gmail.com"),
+                ViewActions.closeSoftKeyboard()
+            )
+
+        Espresso.onView(ViewMatchers.withId(R.id.edt_password_main_activity))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed())).perform(
+                ViewActions.typeText("test@123"),
+                ViewActions.closeSoftKeyboard()
+            )
+
+        Espresso.onView(ViewMatchers.withId(R.id.btn_login_main_activity))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed())).perform(
+                click()
+            )
+        Thread.sleep(1000)
+    }
+}
--- app/src/main/java/com/example/androidcicdexample/MainActivity.kt
+++ app/src/main/java/com/example/androidcicdexample/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.androidcicdexample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_login_main_activity.setOnClickListener {
+            startActivity(Intent(this, DashBoardActivity::class.java))
+            finish()
+        }
     }
 }
