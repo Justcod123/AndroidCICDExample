89fcd943099cdfd246da0b3efef7b3919efeb6af
Justcod123 <himanshu21justcodenow@gmail.com>
2021-10-18 10:09:41 +0530

    Update Font Size

--- app/src/main/java/com/example/androidcicdexample/DashBoardActivity.kt
+++ app/src/main/java/com/example/androidcicdexample/DashBoardActivity.kt
@@ -2,6 +2,10 @@ package com.example.androidcicdexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
+
 
 class DashBoardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
